4f66d303f37da06555c42007fa5b9b52d142eb7e
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-01-17 00:53:51 +0900

    フォーマットしてみました。

--- project/Build.scala
+++ project/Build.scala
@@ -1,12 +1,18 @@
 import sbt._
 
 object ScalaSample002Build extends Build {
-  lazy val root = Project(id = "scala-sample-002",
-    base = file(".")) aggregate(project1, project2)
+  lazy val root = Project(
+    id = "scala-sample-002",
+    base = file(".")
+  ) aggregate(project1, project2)
 
-  lazy val project1 = Project(id = "scala-sample-002-1",
-    base = file("project1"))
+  lazy val project1 = Project(
+    id = "scala-sample-002-1",
+    base = file("project1")
+  )
 
-  lazy val project2 = Project(id = "scala-sample-002-2",
-    base = file("project2"))
+  lazy val project2 = Project(
+    id = "scala-sample-002-2",
+    base = file("project2")
+  )
 }
